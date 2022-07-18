6835dcbadb46c7d7bb716562ef626af968c6180d
yurick1313 <105969551+yurick1313@users.noreply.github.com>
2022-07-18 18:41:37 +0300

    Start final container

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
 	$('.header-slider').bxSlider();
 });
+
+$
+('.item-slider').bxSlider({
+	auto: true,
+	controls: false,
+  pager: true,
+  slideWidth: 400,
+	speed: 700,
+});
+
+$
+('.lable').bxSlider({
+	speed:700 ,
+	minSlides: 1,
+	maxSlides: 12,
+	moveSlides: 0,
+	slideWidth: 0,
+	shrinkItems:false,
+});
